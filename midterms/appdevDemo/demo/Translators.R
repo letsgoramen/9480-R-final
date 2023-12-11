0028b990569e87eef9a3326d2965abc6d54ac552
lody ang <melodyang13@gmail.com>
2023-12-11 20:48:10 +0800

    initial commit

---
+++ midterms/appdevDemo/demo/Translators.R
@@ -0,0 +1,12 @@
+#TRANSLATORS/ INTERPRETERS
+
+# Assignment of value to variable a
+a <- 10
+
+# Condition to determine whether the value of a is equal to 10
+# or not
+if (a == 10) {
+  print("Variable a is equal to 10")
+} else {
+  print("Variable a is not equal to 10")
+}
