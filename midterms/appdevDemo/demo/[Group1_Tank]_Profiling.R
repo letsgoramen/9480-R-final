965b39aa9d85e629f681e447e4d025a72e9397b9
Keith <2221742@slu.edu.ph>
2023-12-14 17:48:18 +0800

    Changed [Group1_Tank]_Debugging.R to conform Assignment, Commenting Guidelines, & Object and Methods.
     R code involves the usage of = for assignment within functions, adhering to the specified standard of utilizing = in other assignment applications. Therefore, the code already conforms to the given modified standard.
    
    The given scripy adheres to the modified standard in R, and it includes inline comments following the specified format.
    
    Code is already in compliance with the modified standard in R and does not involve the use of S4 objects or methods. It follows standard S3 object-oriented programming conventions in R.

--- midterms/appdevDemo/demo/[Group1_Tank]_Profiling.R
+++ midterms/appdevDemo/demo/[Group1_Tank]_Profiling.R
@@ -1,5 +1,4 @@
 #  Copyright (c) 2023 Group1_R
-
 #' @author Rohit Tank
 
 # File Description: Profiling in R
@@ -11,63 +10,63 @@
 # and prints the summary.
 
 # Function to generate a random matrix
-    generate_random_matrix <- function(rows, cols) 
-    {
-      matrix(runif(rows * cols), nrow = rows, ncol = cols);
-    }
+generate_random_matrix <- function(rows, cols)
+{
+  matrix(runif(rows * cols), nrow = rows, ncol = cols);     # generate random matrix;
+}
 
 # Function to perform matrix multiplication
-    matrix_multiply <- function(mat1, mat2) 
-    {
-      if (ncol(mat1) != nrow(mat2)) {
-        stop("Number of columns in the first matrix must be equal to the number of rows in the second matrix.");
-      }
-      return(mat1 %*% mat2);
-    }
+matrix_multiply <- function(mat1, mat2)
+{
+  if (ncol(mat1) != nrow(mat2)) {
+      stop("Number of columns in the first matrix must be equal to the number of rows in the second matrix.");     # check matrix dimensions;
+  }
+  return(mat1 %*% mat2);     # return the result of matrix multiplication;
+}
 
 # Function to calculate the sum of squares of matrix elements
-    sum_of_squares <- function(mat) 
-    {
-      return(sum(mat^2));
-    }
+sum_of_squares <- function(mat)
+{
+  return(sum(mat^2));     # return the sum of squares of matrix elements;
+}
 
 # Function to run the main program
-    main_function <- function() 
-    {
-      # Main program
-      set.seed(123);  # Set seed for reproducibility
+main_function <- function()
+{
+  # Main program
+  set.seed(123);     # Set seed for reproducibility
 
-      # Generate random matrices
-      matrix_size <- 100;
-      A <- generate_random_matrix(matrix_size, matrix_size);
-      B <- generate_random_matrix(matrix_size, matrix_size);
+  # Generate random matrices
+  matrix_size <- 100;
+  A <- generate_random_matrix(matrix_size, matrix_size);
+  B <- generate_random_matrix(matrix_size, matrix_size);
 
-      # Perform matrix multiplication
-      C <- matrix_multiply(A, B);
+  # Perform matrix multiplication
+  C <- matrix_multiply(A, B);
 
-      # Calculate the sum of squares of matrix elements
-      result <- sum_of_squares(C);
+  # Calculate the sum of squares of matrix elements
+  result <- sum_of_squares(C);
 
-      # Iterative algorithm
-      iterations <- 1000
-      for (i in 1:iterations) {
-        A <- matrix_multiply(A, B)
-        B <- generate_random_matrix(matrix_size, matrix_size)
-      }
+  # Iterative algorithm
+  iterations <- 1000;
+  for (i in 1:iterations) {
+    A <- matrix_multiply(A, B);
+    B <- generate_random_matrix(matrix_size, matrix_size);
+  }
 
-      # Print the final result
-      cat("Final Result:", result, "\n");
-    }
+  # Print the final result
+  cat("Final Result:", result, "\n");     # print the final result;
+}
 
-    data <- "profile_data.prof";
+data <- "profile_data.prof";
 
-    Rprof(data);
+Rprof(data);
 
 # Run the main program
-    main_function();
+main_function();
 
-    Rprof();
+Rprof();
 
 # Analyze the profile data
-    summary_data <- summaryRprof(data); # summarizes the retrieved data
-    print(summary_data);
+summary_data <- summaryRprof(data);     # summarizes the retrieved data;
+print(summary_data);     # print the summary data;
