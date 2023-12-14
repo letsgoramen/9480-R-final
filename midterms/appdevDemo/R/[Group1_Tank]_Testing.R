e836d82cee95cb3c7851a301de15cb2008257661
Keith <2221742@slu.edu.ph>
2023-12-14 16:49:39 +0800

    Changed [Group1_Tank]_Testing.R to conform Commenting guidelines

--- midterms/appdevDemo/R/[Group1_Tank]_Testing.R
+++ midterms/appdevDemo/R/[Group1_Tank]_Testing.R
@@ -2,54 +2,53 @@
 
 #' @author Melody Jane Ang
 
-# File Description: 
-#   This program demonstrates matrix multiplication in R. 
-#   It defines functions to generate a random matrix, perform matrix multiplication, 
-#   calculate the sum of squares of matrix elements, and run the main program. 
-#   The main program sets a seed for reproducibility, generates random matrices, 
-#   performs matrix multiplication, calculates the sum of squares, and prints the final result.
+# File Description:
+# This program demonstrates matrix multiplication in R.
+# It defines functions to generate a random matrix, perform matrix multiplication, 
+# calculate the sum of squares of matrix elements, and run the main program.
+# The main program sets a seed for reproducibility, generates random matrices,
+# performs matrix multiplication, calculates the sum of squares, and prints the final result.
 
 
 # Sample Program
 # Function to generate a random matrix
 generate_random_matrix <- function(rows, cols)
 {
-  matrix(runif(rows * cols), nrow = rows, ncol = cols)
+  matrix(runif(rows * cols), nrow = rows, ncol = cols);     # generate random matrix;
 }
 
 # Function to perform matrix multiplication
 matrix_multiply <- function(mat1, mat2)
 {
   if (ncol(mat1) != nrow(mat2)) {
-    stop("Number of columns in the first matrix must be equal to the number of rows in the second matrix.")
+      stop("Number of columns in the first matrix must be equal to the number of rows in the second matrix.");
   }
-  return(mat1 %*% mat2)
+  return(mat1 %*% mat2);     # return the result of matrix multiplication;
 }
 
 # Function to calculate the sum of squares of matrix elements
-  sum_of_squares <- function(mat) 
-  {
-    return(sum(mat^2));
-  }
+sum_of_squares <- function(mat)
+{
+  return(sum(mat^2));     # return the sum of squares of matrix elements;
+}
 
 # Function to run the main program
-  main_function <- function() 
-  {
+main_function <- function()
+{
+  # Main program
+  set.seed(123);     # Set seed for reproducibility;
 
-# Main program
-    set.seed(123);  # Set seed for reproducibility
+  # Generate random matrices
+  matrix_size <- 100
+  A = generate_random_matrix(matrix_size, matrix_size);
+  B = generate_random_matrix(matrix_size, matrix_size);
 
-# Generate random matrices
-     matrix_size <- 100
-     A = generate_random_matrix(matrix_size, matrix_size);
-     B = generate_random_matrix(matrix_size, matrix_size);
+  # Perform matrix multiplication
+  C = matrix_multiply(A, B);
 
-# Perform matrix multiplication
-     C = matrix_multiply(A, B);
+  # Calculate the sum of squares of matrix elements
+  result = sum_of_squares(C);
 
-# Calculate the sum of squares of matrix elements
-     result = sum_of_squares(C);
-
-# Print the final result
-     cat("Final Result:", result, "\n");
-  }
+  # Print the final result
+  cat("Final Result:", result, "\n");
+}
