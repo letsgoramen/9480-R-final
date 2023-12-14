07951b9f81578b66325b9b01bbde5afbbc8a4834
Keith <2221742@slu.edu.ph>
2023-12-14 18:42:35 +0800

    Changed [Group1_Tank]_Translators.R to conform Assignment, Commenting Guides, Function Docu, & Objects/Methods
    The code is already in compliance with the modified standard in R, which involves using " <- " only when assigning functions and using " = " in other assignment applications.
    
    Code is organized as a function create_student_bar_plot that takes a data frame as input and returns the same data frame. Users can use this function by supplying a data frame as input and capturing the result in a variable.
    
     R script follows this convention, and there's no usage of S4 objects. It uses the standard S3 object-oriented system in R.

--- midterms/appdevDemo/demo/[Group1_Tank]_Translators.R
+++ midterms/appdevDemo/demo/[Group1_Tank]_Translators.R
@@ -2,32 +2,42 @@
 
 #' @author Dean Nobleza
 
-# File Description:
-# This program creates a dataset of students, including their names, ages, and grades.
-# It then displays the dataset, imports the ggplot2 package, and creates a bar plot
-# showing the average grades of students.
+# Function Description:
+# This function takes a data frame of students' information,
+# including their names, ages, and grades. It displays the dataset,
+# imports the ggplot2 package, and creates a bar plot showing
+# the average grades of students.
 
-# Necessary Libraries
-library(ggplot2);
+#' @param input_data A data frame containing information about students,
+#'                   including columns 'Name', 'Age', and 'Grade'.
+#' @return A data frame containing the original student information.
+create_student_bar_plot <- function(input_data) {
+  # Display the dataset
+  print(input_data);     # Display the dataset;
+  View(input_data);      # View the dataset;
 
-# Create a students dataset
-    students <- data.frame(
-      Name = c("Rohit", "Shanthal", "Melody", "Ethan", "Christian"),
-      Age = c(21, 24, 21, 20, 21),
-      Grade = c(98, 90, 78, 95, 88)
-    )
+  # Import ggplot2 package
+  library(ggplot2);
 
-# Display the dataset
-    print(students);
-    View(students);
+  # Create a bar plot of average grades by student
+  ggplot(input_data, aes(x = Name, y = Grade, fill = Name)) +  # nolint; Set up the plot aesthetics;
+    geom_bar(stat = "identity") +  # Create the bar plot;
+    labs(title = "Average Grades of Students",  # Add title and axis labels;
+         x = "Student Name",
+         y = "Grade") +
+    theme_classic();     # Apply classic theme;
 
-# import ggplot2 package
-    library(ggplot2);
+  # Return the original data frame
+  return(input_data);
+}
 
-# Create a bar plot of average grades by student
-    ggplot(students, aes(x = Name, y = Grade, fill = Name)) +
-      geom_bar(stat = "identity") +
-      labs(title = "Average Grades of Students",
-          x = "Student Name",
-          y = "Grade") +
-      theme_classic();
+# Example usage:
+# Assuming 'students' is the data frame of student information
+students <- data.frame(
+  Name = c("Rohit", "Shanthal", "Melody", "Ethan", "Christian"),
+  Age = c(21, 24, 21, 20, 21),
+  Grade = c(98, 90, 78, 95, 88)
+)
+
+# Run the function
+result <- create_student_bar_plot(students);   # Run the function with the example data;
