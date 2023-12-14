a4ae64000ad9ef0a07e801a631076854f4bf3e0f
Keith <2221742@slu.edu.ph>
2023-12-14 17:34:08 +0800

    Changed [Group1_Tank]_Debugging.R to conform Assignment, Commenting Guidelines, & Object and Methods.
    note that the specified standard for a data frame may not be applicable to the given code, as the functions deal with scalar values rather than data frames.
    
    R code doesn't involve the use of S4 objects or methods, and it adheres to the specified standard of avoiding S4 objects and methods when possible. Therefore, the code already conforms to the given standard.

--- midterms/appdevDemo/demo/[Group1_Tank]_Debugging.R
+++ midterms/appdevDemo/demo/[Group1_Tank]_Debugging.R
@@ -6,36 +6,39 @@
 # This program demonstrates basic mathematical operations and includes functions
 # for incrementing, decrementing, reciprocating, and calling these functions in a sequence.
 
-
-
 # Increment 'a' by 1
-    inc <- function(a) 
-    {
-      a <- a + 1;
-      return(a);  # Add 'return' to return the incremented value
-    }
+inc = function(a) 
+{
+  a = a + 1;     # Increment 'a' by 1;
+  return(a)
+}
 
 # Decrement 'a' by 1
-    dec <- function(a) 
-    {
-      a <- a - 1; # Correct the decrement operation
-      return(a);
-    }
+dec = function(a) 
+{
+  a = a - 1;     # Decrement 'a' by 1;
+  return(a)
+}
 
 # Reciprocate 'a'
-    reciprocate <- function(a) 
-    {
-      if (a == 0)
-        stop("Division by zero will occur");
-      a <- 1 / a;
-      return(a);
-    }
+reciprocate = function(a) 
+{
+  if (a == 0)
+    stop("Division by zero will occur")
+  a = 1 / a;     # Reciprocate 'a';
+  return(a)
+}
 
 # Define 'myfunc' which calls 'reciprocate', 'inc', and 'dec' functions
-    myfunc <- function(a) 
-    {
-      x <- reciprocate(inc(a));
-      y <- reciprocate(dec(a));
-      result <- x * y;
-      return(result);  # Add 'return' to return the computed result
-    }
+myfunc = function(a) 
+{
+  x = reciprocate(inc(a))      # nolint; Calculate reciprocal of incremented 'a';
+  y = reciprocate(dec(a))      # nolint; Calculate reciprocal of decremented 'a';
+  result = x * y               # Multiply the reciprocals;
+  return(result)
+}
+
+# Example usage:
+a = 5
+result = myfunc(a)
+cat("Result:", result, "\n")
